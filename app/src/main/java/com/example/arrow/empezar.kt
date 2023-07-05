b3d083927d038f98c5c3e94a797283d9defaef5b
Jonathan <213271010@alumnos.utn.edu.mx>
2023-07-05 16:12:43 -0600

    Se agrega el proyecto

---
+++ app/src/main/java/com/example/arrow/empezar.kt
@@ -0,0 +1,11 @@
+package com.example.arrow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class empezar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empezar)
+    }
+}
